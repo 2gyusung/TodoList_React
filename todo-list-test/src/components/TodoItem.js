69dcfead627a2827af384f8546304007a0fba659
2gyusung <leegs005@naver.com>
2025-01-17 12:15:56 +0900

    delete 기능 추가하기

--- todo-list-test/src/components/TodoItem.js
+++ todo-list-test/src/components/TodoItem.js
@@ -2,14 +2,18 @@
 
 import React from 'react'
 
-function TodoItem({id, content, isDone, createDate}) {
+function TodoItem({id, context, isDone, createDate , onUpdate}) {
+  const onChangeCheckbox = () => {
+    onUpdate(id)
+  }
+
   return (
     <div className='TodoItem'>
       <div className='checkbox_col'>
-        <input type='checkbox'/>
+        <input onChange={onChangeCheckbox} checked={isDone} type='checkbox'/>
       </div>
-      <div className='title_col'>할일</div>
-      <div className='date_col'>{new Date().toLocaleDateString()}</div>
+      <div className='title_col'>{context}</div>
+      <div className='date_col'>{new Date(createDate).toLocaleDateString()}</div>
       <div className='btn_col'>
         <button>삭제</button>
       </div>
--- todo-list-test/src/components/TodoList.js
+++ todo-list-test/src/components/TodoList.js
@@ -1,17 +1,30 @@
 // 검색에 따라 필터링된 할 일 아이템 렌더링
 
-import React from 'react'
+import React, { useState } from 'react'
 import TodoItem from './TodoItem'
 
-function TodoList({todo}) {
+function TodoList({todo , onUpdate}) {
+  const [search, setSearch] = useState("")
+
+  const getSearchResult = () => {
+    return search === "" ? todo : todo.filter((it) => it.context.toLowerCase().includes(search.toLowerCase())) //"" 빈 문자열이면 todo를 반환하고 그렇지 않으면 search의 내용과 일치하는 아이템만 필터링해 반환
+  }
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value)
+  }
   return (
     <div className='TodoList'>
       <h4>Todo List</h4>
-      <input className='searchbar' placeholder='검색어를 입력하세요'/>
+      <input 
+      className='searchbar'
+      value={search}
+      onChange={onChangeSearch}
+      placeholder='검색어를 입력하세요'/>
       <div className='list_wrapper'>
       {
-        todo.map((it)=> (
-          <TodoItem {...it} />
+        getSearchResult().map((it)=> (
+          <TodoItem {...it} key={it.id} {...it} onUpdate={onUpdate}/>
         ))
      }
       </div>
